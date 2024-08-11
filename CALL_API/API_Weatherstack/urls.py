6014934af689103b1f3f0b930ef048627a4c4152
KSPIAS <kritsanasonong@gmail.com>
2024-08-11 21:26:29 +0700

    Add API_COVID

--- CALL_API/API_Weatherstack/urls.py
+++ CALL_API/API_Weatherstack/urls.py
@@ -3,5 +3,6 @@ from API_Weatherstack import views
 
 urlpatterns = [
     path('',views.index),
-    path('API_Weatherstack',views.API_Weatherstack)
+    path('API_Weatherstack',views.API_Weatherstack),
+    path('API_COVID',views.API_COVID)
 ]
--- CALL_API/API_Weatherstack/views.py
+++ CALL_API/API_Weatherstack/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
+# import json
 
 # Create your views here.
 def index(request):
@@ -6,3 +8,8 @@ def index(request):
 
 def API_Weatherstack(request):
     return render(request,"API_Weatherstack.html")
+
+def API_COVID(request):
+    response = requests.get('https://covid-api.com/api/regions').json()
+    # response = json.loads(response)
+    return render(request,"API_COVID.html",{'response':response})
