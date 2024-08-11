4bf3228b60b3c36532e15ae8e9eb5ffc7768cea8
KSPIAS <kritsanasonong@gmail.com>
2024-08-11 14:56:24 +0700

    Add file:API_Weatherstack.html,base.html

--- CALL_API/API_Weatherstack/urls.py
+++ CALL_API/API_Weatherstack/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from API_Weatherstack import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('API_Weatherstack',views.API_Weatherstack)
 ]
