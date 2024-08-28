9a5a37565518b5b91eb620561b415f85c2865a25
KSPIAS <kritsanasonong@gmail.com>
2024-08-28 19:12:25 +0700

    Add PK Table/Models

--- weatherstack/models.py
+++ weatherstack/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.utils import timezone
+from django.utils import timezone
 
 # Create your models here.
 # class apilogin(models.Model):
@@ -19,7 +19,8 @@ class apilogin(models.Model):
         return self.name
 
 class api_request(models.Model):
-    city = models.CharField(max_length=100 ,primary_key=True)
+    request_id = models.CharField(max_length=100, primary_key=True)
+    city = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     language = models.CharField(max_length=20)
     unit = models.CharField(max_length=10)
@@ -29,6 +30,6 @@ class api_request(models.Model):
     class Meta:
         db_table = 'api_request'
         managed = False
-    
+
     def __str__(self):
-        return self.city
+        return self.request_id
--- weatherstack/views.py
+++ weatherstack/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 import requests
-from weatherstack.models import apilogin #,api_request
+from weatherstack.models import apilogin ,api_request
 from datetime import datetime, timezone, timedelta
 
 
 # Create your views here.
 def API_Weatherstack(request):
-    query = "Thailand"
-    result = w_request(query)
+    city = "Thailand"
+    result = w_request(city)
     return render(request,"API_Weatherstack.html",{'get_api':result})
 
-def w_request(query):
+def w_request(city):
     try:
         current_datetime = datetime.now().date()
         current_month = datetime.now().month
@@ -18,10 +18,10 @@ def w_request(query):
         total_in_month = api_request.objects.filter(data_date__month=current_month).filter(data_date__year=current_year).count()
         print(f"total_call_in_month: {total_in_month}")
         # get_api_request = api_request.objects.filter(query="Bangkok, Thailand").filter(data_date__date=current_datetime)
-        get_api_request = api_request.objects.filter(query__icontains=query).filter(data_date__date=current_datetime)
+        get_api_request = api_request.objects.filter(city__icontains=city).filter(data_date__date=current_datetime)
         for i in get_api_request:
             type = i.type
-            location = i.query
+            location = i.city
             language = i.language
             unit = i.unit
             data_date = i.data_date
@@ -32,6 +32,7 @@ def w_request(query):
         return result
     except:
         data_date = datetime.now()
+        data_date_id = data_date.date()
         data_date = data_date.isoformat()
         get_apilogin = apilogin.objects.filter(name="weatherstack_current")
         city = "Bangkok"
@@ -43,7 +44,11 @@ def w_request(query):
         if total_in_month < 245:
             response = requests.get(get_api).json()
             js_request = response['request']
-            api_request.objects.create(**js_request ,data_date=data_date)
+            # print(js_request)
+            js_request_new = js_request.pop('query')
+            js_request['city'] = js_request_new
+            print(js_request)
+            api_request.objects.create(request_id=city+":"+str(data_date_id),**js_request ,data_date=data_date)
             result = "Insert request success"
         else:
             result = "Usage limit has been reached"
